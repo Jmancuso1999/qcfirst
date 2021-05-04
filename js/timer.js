b38136d879059a270a938700436daa3b6820f50e
James Mancuso <Jmancuso1999@gmail.com>
2021-05-04 13:32:56 -0400

    Testing MongoDB

---
+++ js/timer.js
@@ -0,0 +1,34 @@
+// Source to code/class was from a unknown user on https://knowledgecornor.blogspot.com/
+// https://knowledgecornor.blogspot.com/2013/08/birthday-dropdown-using-html-and-js.html
+
+function call(){
+    var kcyear = document.getElementsByName("year")[0],
+     kcmonth = document.getElementsByName("month")[0],
+     kcday = document.getElementsByName("day")[0];
+          
+    var d = new Date();
+    var n = d.getFullYear();
+    for (var i = n; i >= 1950; i--) {
+        var opt = new Option();
+        opt.value = opt.text = i;
+        kcyear.add(opt);
+    }
+    kcyear.addEventListener("change", validate_date);
+    kcmonth.addEventListener("change", validate_date);
+   
+    function validate_date() {
+        var y = +kcyear.value, m = kcmonth.value, d = kcday.value;
+        if (m === "2")
+            var mlength = 28 + (!(y & 3) && ((y % 100) !== 0 || !(y & 15)));
+        else var mlength = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][m - 1];
+        kcday.length = 0;
+        for (var i = 1; i <= mlength; i++) {
+            var opt = new Option();
+            opt.value = opt.text = i;
+            if (i == d) opt.selected = true;
+            kcday.add(opt);
+        }
+    }
+    
+    validate_date();
+}
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
-const express  = require("express");
+const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+
 
 mongoose.connect("mongodb+srv://qcfirst-admin:D3WkB9rtzMiSMfOa@qcfirst.cfs3k.mongodb.net/qcfirst", 
     {useNewUrlParser: true}, 
@@ -31,7 +37,7 @@ app.get("/", function(req, res) {
 })
 
 // POST new_USER
-app.post("/", function(req, res) {
+app.post("/create", function(req, res) {
     let newUser = new user({
         userName: req.body.userN,
         fullName:req.body.fullName,
@@ -43,7 +49,7 @@ app.post("/", function(req, res) {
     
     newUser.save();
     console.log("User Created");
-    res.redirect('/');
+    res.redirect('/html/studentHome.html');
 })
 
 
